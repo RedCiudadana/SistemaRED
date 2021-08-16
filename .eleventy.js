4b1ca3057f84dcbe7b8f3488891a3d9ec18bd49d
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2021-08-16 13:38:15 -0600

    mejoras proyecto

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
 
+const proyectos = [
+    "1",
+    "2",
+    "3"
+];
+
 
 module.exports = function (eleventyConfig) {
 
@@ -6,21 +12,21 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('assets');
     eleventyConfig.addPassthroughCopy('features');
-    /*
-    instituciones.forEach((institucion) => {
-        eleventyConfig.addCollection(institucion, function (collectionApi) {
+    
+    proyectos.forEach((proyecto) => {
+        eleventyConfig.addCollection(proyecto, function (collectionApi) {
 
-            let collection = collectionApi.getFilteredByTags('datos').filter(function (item) {
-                return item.data.datos.Institucion === institucion;
+            let collection = collectionApi.getFilteredByTags('proyectos').filter(function (item) {
+                return item.data.proyectos.id_proyecto === proyecto;
             });
 
             return collection;
         });
-    });*/
-
-    eleventyConfig.addCollection("proyectos", function(collectionApi) {
-        return collectionApi.getFilteredByTag('proyectos');
     });
+
+    /*eleventyConfig.addCollection("proyectos", function(collectionApi) {
+        return collectionApi.getFilteredByTag('proyectos');
+    });*/
 
     eleventyConfig.addCollection("monitoreos", function(collectionApi) {
         return collectionApi.getFilteredByTag('monitoreos');
